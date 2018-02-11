321df26853d559df5ae2a3b816d4919193f8f394
Sean Thielen <sean@p27.us>
2018-02-11 10:33:11 -0800

    Initial

---
+++ GVRSDK_Wrapper.framework/Headers/GVRSDK_Wrapper.h
@@ -0,0 +1,20 @@
+//
+//  GVRSDK_Wrapper.h
+//  GVRSDK-Wrapper
+//
+//  Created by Sean Thielen on 2/11/18.
+//  Copyright © 2018 GoMeta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GVRSDK_Wrapper.
+FOUNDATION_EXPORT double GVRSDK_WrapperVersionNumber;
+
+//! Project version string for GVRSDK_Wrapper.
+FOUNDATION_EXPORT const unsigned char GVRSDK_WrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GVRSDK_Wrapper/PublicHeader.h>
+#import "GVRPanoramaView.h"
+#import "GVRWidgetView.h"
+#import "GVRVideoView.h"
